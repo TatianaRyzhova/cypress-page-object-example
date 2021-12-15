59378e4331429980c0f0500b0eac7b721985e696
Tatiana Ryzhova <tryzhova93@gmail.com>
2021-12-15 19:25:31 +0100

    refactoring

--- cypress/support/pages/form.js
+++ cypress/support/pages/form.js
@@ -1,41 +1,59 @@
-const NAME = '#name';
-const EMAIL = '#email';
-const MESSAGE = '#message';
+const NAME_INPUT = '#name';
+const EMAIL_INPUT = '#email';
+const MESSAGE_INPUT = '#message';
 const SUBMIT_BUTTON = '#submitButton';
 const LOADER = '#loader';
 
 export class Form {
-
-  get nameField() {
-    return cy.get(NAME);
-  }
-
-  get emailField() {
-    return cy.get(EMAIL);
-  }
-
-  get messageField() {
-    return cy.get(MESSAGE);
-  }
-
-  get loader() {
-    return cy.get(LOADER);
-  }
-
   visit() {
     cy.log('Visiting main page');
     cy.visit('/');
     return this;
   }
 
+  fillName(name) {
+    cy.log('Filling name');
+    cy.get(NAME_INPUT).clear().type(name);
+    return this;
+  }
+
+  fillEmail(email) {
+    cy.log('Filling email');
+    cy.get(EMAIL_INPUT).clear().type(email);
+    return this;
+  }
+
+  fillMessage(message) {
+    cy.log('Filling message');
+    cy.get(MESSAGE_INPUT).clear().type(message);
+    return this;
+  }
+
   fillTheForm(data) {
-    cy.get(NAME).clear().type(data.name);
-    cy.get(EMAIL).clear().type(data.email);
-    cy.get(MESSAGE).clear().type(data.message);
+    this.fillName(data.name);
+    this.fillEmail(data.email);
+    this.fillMessage(data.message);
   }
 
   submitTheForm() {
+    cy.log('Submitting form')
     cy.get(SUBMIT_BUTTON).click();
+  }
+
+  get nameField() {
+    return cy.get(NAME_INPUT);
+  }
+
+  get emailField() {
+    return cy.get(EMAIL_INPUT);
+  }
+
+  get messageField() {
+    return cy.get(MESSAGE_INPUT);
+  }
+
+  get loader() {
+    return cy.get(LOADER);
   }
 }
 
